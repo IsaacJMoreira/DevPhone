6950d0f417ecdefda46378585440b6d75b9b5f7b
Valfrido Santiago <valfrido.santiago@hotmail.com>
2023-08-29 15:24:49 -0300

    alteration user middleware

--- BACK/MVP_BACK/src/infra/middlewares/user.ts
+++ BACK/MVP_BACK/src/infra/middlewares/user.ts
@@ -1,10 +1,12 @@
 const {validate, Joi } = require ("express-validation");
-
+const userdata = require ("../../modules/User/controller")
 
 const validatePost = validate({
     body: Joi.object().keys ({
         nome: Joi.string().required(),
         email: Joi.string().required(),
+        credential: Joi.string().required(),
+        password: Joi.string().min(6).max(20).required()
     })
 })
 
@@ -12,7 +14,9 @@ const validateUpdate = validate({
     body: Joi.object().keys ({
         nome: Joi.string().required(),
         email: Joi.string().required(),
-        passaword: Joi.string().required()
+        credential: Joi.string().required(),
+        passaword: Joi.string().min(6).max(20).required(),
+        Adreess: Joi.IAddress().required()
     })
 })
 
