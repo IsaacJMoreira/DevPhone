162a9428861bbdfd741d186f92f8d880a97b9d56
Valfrido Santiago <valfrido.santiago@hotmail.com>
2023-08-31 16:09:27 -0300

    init tests unit

---
+++ BACK/MVP_BACK/src/modules/User/tests/index.ts
@@ -0,0 +1,4 @@
+import { User } from "../../../models";
+import controller from "../controller"; 
+
+export const controller = new controller(User); 
---
+++ BACK/MVP_BACK/src/modules/User/tests/user-tests.spec.ts
@@ -0,0 +1,26 @@
+import { number } from "joi";
+import { controller } from "./index";
+
+describe('User crud repository', ()=>{
+    test('create user and save in DB', async ()=> {
+
+        const payloadCreate = {
+        name:'Teste',
+        email:'Teste@email',
+        credential:'adm',
+        password:'12345',
+        };
+
+        const payloadUpdate = {
+        name:'Teste',
+        email:'Teste@email',
+        credential:'adm',
+        password:'12345',
+        };
+
+        const result = await controller.create(payloadCreate,payloadUpdate) 
+    
+        expect(result).toBeTruthy()
+        expect(typeof result.id === 'number').toBeTruthy()
+    })
+})
