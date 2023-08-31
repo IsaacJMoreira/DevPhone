223007b40725a095188bf0955473a03a8e3c9d00
mpatyele <mariana01.pp@gmail.com>
2023-08-31 20:38:13 +0300

    Page Shop- alterando para trycatch

--- FRONT/MVP_FRONT/src/Pages/Components/Active/CategorieArea/index.tsx
+++ FRONT/MVP_FRONT/src/Pages/Components/Active/CategorieArea/index.tsx
@@ -1,58 +1,54 @@
-import * as React from 'react'
-import CategoriesUL from '../CategoriesUL'
-import axios from 'axios'
-import baseURL from '../../../../../baseURL'
-
+import * as React from 'react';
+import CategoriesUL from '../CategoriesUL';
+import axios from 'axios';
+import baseURL from '../../../../../baseURL';
 
 type Categorie = {
     _id: string;
     name: string;
-    code: string
-}
+    code: string;
+};
 
-export const CategorieArea = ()=>{
-
-
+export const CategorieArea = () => {
     const [categories, setCategories] = React.useState<Categorie[]>([]);
-    //usar async await - aulas
-    React.useEffect ( ()=>{
-        axios.get<Categorie[]>(`${baseURL}/categories`).then((response)=>{
 
+    const fetchData = async () => {
+        try {
+            const response = await axios.get<Categorie[]>(`${baseURL}/categories`);
             setCategories(response.data);
-        } )
-
-        .catch(error=>{
+        } catch (error) {
             console.log("Error fetching data", error);
-        });
-    }, [])
+        }
+    };
 
-    if(!categories) return (
-        <>
-            <h6>Sorry, nothing to select here 😥</h6>
-        </>
-    );
+    React.useEffect(() => {
+        fetchData();
+    }, []);
+
+    if (!categories.length) {
+        return (
+            <>
+                <h6>Sorry, nothing to select here 😥</h6>
+            </>
+        );
+    }
 
     return (
         <>
-            
-          <ul>
-            <div>
-                <h3>Categories</h3> 
-            </div>  
-          
-          <br />
-            {categories.map((categorie: Categorie)=>{
-                        return(                               
-                            <CategoriesUL
-                                key = {categorie._id} 
-                                id = {`i${categorie._id}`}
-                                name = {categorie.name}
-                            />                    
-                           );   
-                         })
-            } 
-            </ul>    
-        </>      
-    ); 
-    
+            <ul>
+                <div>
+                    <h3>Categories</h3>
+                </div>
+
+                <br />
+                {categories.map((categorie: Categorie) => (
+                    <CategoriesUL
+                        key={categorie._id}
+                        id={`i${categorie._id}`}
+                        name={categorie.name}
+                    />
+                ))}
+            </ul>
+        </>
+    );
 };
