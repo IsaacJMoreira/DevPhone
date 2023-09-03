52f945cfd7a0e2ec5c132a2a0a4612a3ccbd9383
mpatyele <mariana01.pp@gmail.com>
2023-09-03 13:46:40 +0300

    Ajuste links page Home

--- FRONT/MVP_FRONT/src/Pages/Components/Active/HomeShopArea/index.tsx
+++ FRONT/MVP_FRONT/src/Pages/Components/Active/HomeShopArea/index.tsx
@@ -3,45 +3,47 @@ import { ProductsHome } from '../../ProductsHome'
 import axios from 'axios'
 import baseURL from '../../../../../baseURL'
 import { CardProd } from './styles'
+import { Link } from 'react-router-dom'
 
 type Product = {
-    _id: string;
-    imgURL: string;
-    alt: string;
-    name: string;
-    shortDescription: string;
+  _id: string;
+  imgURL: string;
+  alt: string;
+  name: string;
+  shortDescription: string;
 
 }
 export const HomeShopArea = () => {
-    const [products, setProducts] = React.useState<Product[]>([]);
-    const productsToShow = 3;
+  const [products, setProducts] = React.useState<Product[]>([]);
+  const productsToShow = 3;
 
-    const fetchData = async () => {
-        try {
-          const response = await axios.get<Product[]>(`${baseURL}/product/allproducts`);
-          setProducts(response.data);
-        } catch (error) {
-          console.log('Error fetching data', error);
-        }
-      };
-    
-      React.useEffect(() => {
-        fetchData();
-      }, []);
-    
-      if (!products.length) {
-        return (
-          <>
-            <h6>Sorry, nothing to buy here</h6>
-          </>
-        );
-      }
-    
-  
-  
+  const fetchData = async () => {
+    try {
+      const response = await axios.get<Product[]>(`${baseURL}/product/allproducts`);
+      setProducts(response.data);
+    } catch (error) {
+      console.log('Error fetching data', error);
+    }
+  };
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (!products.length) {
     return (
       <>
-        {products.slice(0, productsToShow).map((product: Product) => (
+        <h6>Sorry, nothing to buy here</h6>
+      </>
+    );
+  }
+
+
+
+  return (
+    <>
+      {products.slice(0, productsToShow).map((product: Product) => (
+        <Link to={`/product/${product._id}`} key={product._id}>
           <CardProd key={product._id}>
             <ProductsHome
               key={`${product._id}`}
@@ -51,7 +53,8 @@ export const HomeShopArea = () => {
               Description={product.shortDescription}
             />
           </CardProd>
-        ))}
-      </>
-    );
-  };
+        </Link>
+      ))}
+    </>
+  );
+};
