3fc4c6a9c050eb37c88bd1d4379bff2576b29ecd
Valfrido Santiago <valfrido.santiago@hotmail.com>
2023-09-04 12:04:51 -0300

    verifications middlewares order/categorie

--- BACK/MVP_BACK/src/infra/middlewares/categorie.ts
+++ BACK/MVP_BACK/src/infra/middlewares/categorie.ts
@@ -9,10 +9,34 @@ const category = validate ({
     }).required()
 })
 
+const validateCreate = validate({
+    body :Joi.object().keys({
+        code: Joi.string().min(3).max(30).required(),
+        name: Joi.string().min(3).max(30).required(),
+        fatherCategory: Joi.string().min(1).max(30),
+    })
+})
+
+const validateUp = ({
+    body: Joi.object().keys({
+        code: Joi.string ().optional (),
+        nome: Joi.strig().min(3).max(30).required(),
+        fatherCategory: Joi.string().min(1).max(30),
+    })
+})
+
+
 const middlewareCat = {
-    categoryValidation : async (req:Request , res: Response, next: NextFunction) => {
+    findallCategorie: async (req:Request , res: Response, next: NextFunction) => {
         await category (req,res,next)
+    },
+    createCategorie : async (req:Request , res: Response, next: NextFunction) => {
+        await validateCreate (req,res,next)
+    },
+    updateCategorie: async (req:Request , res: Response, next: NextFunction) => {
+        await validateUp (req,res,next)
     }
+
 }
 
 export default middlewareCat;  
--- BACK/MVP_BACK/src/infra/middlewares/order.ts
+++ BACK/MVP_BACK/src/infra/middlewares/order.ts
@@ -5,9 +5,17 @@ const validateOrder = validate ({
     body: Joi.object().keys({
         ownerID: Joi.string().required(),
         itens: Joi.string().required(),
-        orderName :Joi.string().min(3).max(20).required()
+        //orderName :Joi.string().min(3).max(20).required() "ideia para add no fluxo" 
     })
 })
+
+const validateCreate = validate({ 
+    body: Joi.object().keys({
+        ownerID: Joi.string().required(),
+        itens: Joi.strig().required()
+    })
+})
+
 
 const validateUpdate = validate ({
     body: Joi.object().keys({
@@ -22,9 +30,13 @@ const middlewareOrder = {
     findall: async (req:Request,res:Response,next:NextFunction) =>{
         await validateOrder(req,res,next)
     },
+    create: async (req:Request,res:Response,next:NextFunction) =>{
+        await validateCreate(req,res,next)
+    },
     update: async (req:Request,res:Response,next:NextFunction) =>{
         await validateUpdate(req,res,next)
     }
+    
 }
 
 
