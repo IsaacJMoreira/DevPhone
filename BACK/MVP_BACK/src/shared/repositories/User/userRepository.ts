756a668de2188aba6d91bec2385f42762edbcf17
Valfrido Santiago <valfrido.santiago@hotmail.com>
2023-09-04 11:03:33 -0300

    modify

--- BACK/MVP_BACK/src/shared/repositories/User/userRepository.ts
+++ BACK/MVP_BACK/src/shared/repositories/User/userRepository.ts
@@ -1,4 +1,6 @@
+import { promises } from "dns"
 import User from "../../../modules/User/controller"
+import { error } from "console"
 
 export default class userRepository implements IRepository{
     model: userModel 
@@ -10,5 +12,8 @@ export default class userRepository implements IRepository{
     async create(payload: Omit<userPayload, 'create'| 'update'>): promise<user>{
        const result = await this.model.create({...payload, create:new Date().toISOString()})
        return result
+    }
+    async update(): promise<void>{
+        throw new Error("Merhod not implement")
     }
 }
