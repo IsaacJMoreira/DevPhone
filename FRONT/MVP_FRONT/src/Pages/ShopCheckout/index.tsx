a25a7c33597cbd85b25f9a449208503610eefc63
Yosef Egito <yoegito@gmail.com>
2023-09-09 14:09:46 -0300

    Adicionando componentes na página ShopCheckout

--- FRONT/MVP_FRONT/src/Pages/ShopCheckout/index.tsx
+++ FRONT/MVP_FRONT/src/Pages/ShopCheckout/index.tsx
@@ -1,16 +1,44 @@
-import { BodySection } from "./styled";
+import { BodySectionsContainer,
+    BodySectionOne,
+    BodySectionTwo,
+    Line } from "./styled";
+import { Link } from "react-router-dom";
 
-import HeaderMenu from "../Components/HeaderMenu"
-import Footer from "../Components/Footer"
+import HeaderMenu from "../Components/HeaderMenu";
+import Footer from "../Components/Footer";
+import ButtonShop from "../Components/Buttons/ButtonShop";
+import { HorizontalProduct } from "../Components/HorizontalProductCard";
 
-export default function ShopCheckout() {
-    return <>
-        <HeaderMenu />
-            <BodySection>
-                <div>
-                    <h1>{"Pagamento"}</h1>
-                </div>
-            </BodySection>   
-        <Footer />
-    </>
+// Importações de imagens
+import MotoG from "../../img/moto G antigo.jpg";
+import MotoE from "../../img/Moto E antigo.png";
+
+
+export default function ShopCart() {
+return <>
+   <HeaderMenu />
+
+       <BodySectionsContainer>
+       <BodySectionOne>
+           <div>
+               <h1>{"Finalize suas Compras"}</h1>
+               <p>Escolha o frete</p>
+
+               <Link to="/shopCheckout">
+               <ButtonShop />
+               </Link>
+            </div>    
+       </BodySectionOne>
+       <BodySectionTwo>
+           <div>
+               <h1>Sua lista de pedidos</h1>
+               <div className="boxOne"><HorizontalProduct productName={"Moto G"} categoria={"Family G"} quantidade={10} preco={"R$1000"} imageUrl={MotoG}></HorizontalProduct></div>
+               <Line />
+               <div className="boxTwo"><HorizontalProduct productName={"Moto E"} categoria={"Family E"} quantidade={100} preco={"R$500,00"} imageUrl={MotoE}></HorizontalProduct></div>
+               
+           </div>
+       </BodySectionTwo> 
+       </BodySectionsContainer>  
+   <Footer />
+</>
 }
