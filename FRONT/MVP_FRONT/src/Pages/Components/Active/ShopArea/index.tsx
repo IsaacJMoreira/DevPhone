93c583d129de87cef86b42a6a14a26af9d539b64
Isaac J Moreira <120274547+IsaacJMoreira@users.noreply.github.com>
2023-09-13 09:36:09 -0300

    EDIT: shop area now har categories filter

--- FRONT/MVP_FRONT/src/Pages/Components/Active/ShopArea/index.tsx
+++ FRONT/MVP_FRONT/src/Pages/Components/Active/ShopArea/index.tsx
@@ -21,27 +21,31 @@ type Product = {
 
 interface ISearch {
     searchTerm?: string;
+    categoriesList?: string[];
 }
 
 
 export const ShopArea: React.FC<ISearch> = ({
     searchTerm,
+    categoriesList
 }) => {
 
+    const [productsTotal, setProductsTotal] = React.useState(0);
     const [productsList, setProductsList] = React.useState<Product[]>([]);
     const [page, setPage] = React.useState(1);
     const [totalPages, setTotalPage] = React.useState(1);
     const [status, setStatus] = React.useState(200);
+    const [categories, setCategories] = React.useState([]);
     const perPage = 8;
 
     const getProductList = async () => {
         try {
-            const list = await axios.get(`${baseURL}/products/?page=${page}&perPage=${perPage}&searchTerm=${searchTerm}`);
+            const categoriesToSearch = toQueryString(categories);
+            const list = await axios.get(`${baseURL}/products/?page=${page}&perPage=${perPage}&searchTerm=${searchTerm}${categoriesToSearch}`);
             setProductsList(list.data.products);
+            setProductsTotal(list.data.totalProducts)
             setStatus(list.status);
-
-
-            setTotalPage(list.data.totalPages)
+            setTotalPage(list.data.totalPages);
         } catch (error) {
             setStatus(404);
         }
@@ -49,11 +53,21 @@ export const ShopArea: React.FC<ISearch> = ({
     }
 
 
+    const handleCheck = (event) => {
+        var categoriesArray = [...categories];
+        if (event.target.checked) {
+            categoriesArray = [...categories, event.target.value];
+        } else {
+            categoriesArray.splice(categories.indexOf(event.target.value), 1);
+        }
+        setCategories(categoriesArray);
+
+    };
+
+
+
     // Invoke when user click to request another page.
     const handlePageClick = (event: { selected: number }) => {
-        console.log(
-            `User requested page number ${event.selected}`
-        );
         setPage(event.selected + 1);
     };
 
@@ -65,51 +79,67 @@ export const ShopArea: React.FC<ISearch> = ({
 
         getProductList();//gets the first page to be rendered
 
-
-    }, [searchTerm]);
-
+    }, [searchTerm, categories, page]);
+    
+    
     return (
 
         <>
             <div className="CategorieSection">
-                <CategorieArea />
+                <CategorieArea 
+                onChangeFunction={handleCheck}
+                />
             </div>
             <CardContainer>
                 <div>
-                    <h1>{searchTerm ? `Resultados para: "${searchTerm}"` : "Todos os Produtos"}</h1>
+                    <h1>{
+                        searchTerm ?
+                            `Resultados para: "${searchTerm}"` :
+                            "Todos os Produtos"
+                    }
+                    </h1>
                     <div>
-                        <h2>{status === 404 ? "Nenhum resultado encontrado" : ""}</h2>
+                        <h2>{
+                            status !== 200 ?
+                                "💔😭Nenhum resultado encontrado😭💔" :
+                                (
+                                    searchTerm ?
+                                        `📱Total: ${productsTotal} produto${productsTotal > 1 ? "s" : ""}📱` :
+                                        ""
+                                )
+                        }
+                        </h2>
                     </div>
                     <br />
                 </div>
                 {productsList.map((product: Product) => {
                     return (
                         status === 200 &&
-                            <ProductCard
-                                key={`${product._id}`}
-                                Src={product.imgURL}
-                                Alt={product.alt}
-                                Title={product.name}
-                                Price={product.price}
-                                Description={product.shortDescription}
-                                Stock={product.stock}
-                                link={product._id}
-                            />
+                        <ProductCard
+                            key={`${product._id}`}
+                            Src={product.imgURL}
+                            Alt={product.alt}
+                            Title={product.name}
+                            Price={product.price}
+                            Description={product.shortDescription}
+                            Stock={product.stock}
+                            link={product._id}
+                        />
                     )
                 })}
 
                 {
-                status === 200 && <div>
-                    <Paginate
-                        breakLabel="..."
-                        nextLabel="next >"
-                        onPageChange={handlePageClick}
-                        pageRangeDisplayed={5}
-                        pageCount={totalPages}
-                        previousLabel="< previous"
-                        renderOnZeroPageCount={null}
-                    />
-                </div>}
+                    status === 200 && <div>
+                        <Paginate
+                            breakLabel="..."
+                            nextLabel="next >"
+                            onPageChange={handlePageClick}
+                            pageRangeDisplayed={5}
+                            pageCount={totalPages}
+                            previousLabel="< previous"
+                            renderOnZeroPageCount={null}
+                        />
+                    </div>}
 
             </CardContainer>
 
@@ -122,4 +152,12 @@ export const ShopArea: React.FC<ISearch> = ({
     );
 
 
+}
+
+function toQueryString(rawArray: string[]): string {
+    let result = "";
+    rawArray.forEach((element, index) => {
+        result += `&categories%5B${index}%5D=${element}`
+    });
+    return result;
 }
