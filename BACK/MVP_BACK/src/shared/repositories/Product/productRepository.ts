b71c085b18e468a054580917535b9aef674991a1
Valfrido Santiago <valfrido.santiago@hotmail.com>
2023-09-14 21:44:00 -0300

    up

--- BACK/MVP_BACK/src/shared/repositories/Product/productRepository.ts
+++ BACK/MVP_BACK/src/shared/repositories/Product/productRepository.ts
@@ -2,30 +2,34 @@ import { Product } from "../../../models";
 import { ModelStatic } from "sequelize";
 import IRepository from "../../../infra/core/IRepository";
 
+export default class ProductRepository implements IRepository {
+  model: ModelStatic<Product>;
 
-export default class ProductRepository implements IRepository{
-    
-   model:ModelStatic<typeof Product>
-   
-   constructor(model: ModelStatic<typeof Product>){
-    this.model = model
-}
-async create (): Promise<void>{
-}
-async update (): Promise<void>{
+  constructor(model: ModelStatic<Product>) {
+    this.model = model;
+  }
+  async create(payload: Partial<Product>): Promise<Product> {
+    const result = await this.model.create({
+      ...payload,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    });
+    return result;
+  }
+  async update(): Promise<void> {
     throw new Error("Method not implemented.");
-}
-async findone (): Promise<void>{
+  }
+  async findone(): Promise<void> {
     throw new Error("Method not implemented.");
-}
-async findall (): Promise<void>{
+  }
+  async findall(): Promise<void> {
     throw new Error("Method not implemented.");
-}
-async delete(): Promise<void>{
+  }
+  async delete(): Promise<void> {
     throw new Error("Method not implemented.");
-}
-async existByid (): Promise<void>{
-    const result = await Product.count({where: {}})
-    return result
-}
+  }
+  async existByid(): Promise<void> {
+    const result = await Product.count({ where: {} });
+    return result;
+  }
 }
